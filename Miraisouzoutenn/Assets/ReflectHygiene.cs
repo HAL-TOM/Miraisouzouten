bc4cdc2848b9eeb7e5b058a7afef4976ba4f62cc
LAPTOP-MURHC6CL\gamet <halsyuukatutom@gmail.com>
2021-10-25 18:58:34 +0900

    10月25日実装分、光の実装待ち

--- Miraisouzoutenn/Assets/ReflectHygiene.cs
+++ Miraisouzoutenn/Assets/ReflectHygiene.cs
@@ -4,44 +4,77 @@ using UnityEngine;
 
 public class ReflectHygiene : MonoBehaviour
 {
-    /*
-    class InLight
-    {
-
-    };
-    public List<InLight> inLights;
-    */
+    
+  
+    [SerializeField]  private List<InLight> inLights;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-
+        inLights = new List<InLight>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        InitVec();
-        UpdateRef();
+
+         UpdateLight();
     }
-    private void InitVec()
+    private void UpdateLight()
     {
-
+        foreach (InLight light in inLights)
+        {
+            if(Vector3.Dot(transform.up,light.inLight.m_Direction) < 0)
+            {
+                DestoroyLight(light);
+                continue;
+            }
+            light.outLight.SetDirection(RefVec(light.inLight.m_Direction));
+        }
     }
-
-    private void UpdateRef()
+    public void HitLight(SunLight inL)//光が当たった時に呼ばれる
     {
-
+        if(Vector3.Dot(transform.up, inL.m_Direction)>0)
+        {
+            Debug.Log("HitLight");
+            inLights.Add(new InLight(inL, ReflectLight(inL)));
+        }
     }
-    public void AddLight()
+    public void OutLight(SunLight outL)//光が離れた時に呼ばれる
     {
-
+        foreach (InLight light in inLights)
+        {
+            if (light.inLight == outL)
+            {
+                DestoroyLight(light);
+                return;
+            }
+        }
     }
-    public void DestoroyLight()
+
+    private void DestoroyLight(InLight light)
     {
+        //light.outL;//消える処理
+        inLights.Remove(light); // 要素の削除
+    }
 
+    private SunLight ReflectLight(SunLight inL)
+    {
+        SunLight retL = new SunLight();
+        //生成
+        retL.SetAll(gameObject, RefVec(inL.m_Direction), 1.0f, 1.0f);
+        
+        return retL;
+    }
+
+    private Vector3 RefVec(Vector3 inV)
+    {
+        Vector3 normal_n=Vector3.zero;
+        normal_n = Vector3.Normalize(transform.up);//法線ベクトル（GameObjectの上ベクトル）
+        return Vector3.Normalize(inV - 2.0f * Vector3.Dot(inV, normal_n) * normal_n);
+       
     }
 
 
-    
 }
